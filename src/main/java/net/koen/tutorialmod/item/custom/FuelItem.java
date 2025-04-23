cc72c04e974aca86a326057844b0ae0ed68cd759
Koen-mods <Koen.veentjer@gmail.com>
2025-04-23 12:19:14 +0200

    strawberry properties updated

---
+++ src/main/java/net/koen/tutorialmod/item/custom/FuelItem.java
@@ -0,0 +1,12 @@
+package net.koen.tutorialmod.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class FuelItem extends Item {
+    private int burnTime
+
+    public FuelItem(Properties pProperties) {
+        super(pProperties);
+        this.burnTime = burnTime
+    }
+}
